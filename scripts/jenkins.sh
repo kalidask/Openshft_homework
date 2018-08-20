d8a473967830b903cb29941888b033748728d019
root <root@bastion.3cb7.example.opentlc.com>
2018-08-20 12:06:20 +0000

    jenkin update

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -2,4 +2,4 @@ oc new-project cicd
 oc new-app jenkins-ephemeral -n cicd
 oc new-app https://github.com/wkulhanek/openshift-tasks.git --name=openshift-task -n cicd
 oc create -f /opt/Openshft_homework/scripts/sample-pipeline -n cicd
-oc start-build sample-pipeline -n ci-cd
+oc start-build sample-pipeline -n cicd
