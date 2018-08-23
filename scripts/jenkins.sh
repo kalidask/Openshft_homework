77e9a7ecba5109803476584bca891a6e33c65feb
root <root@master1.3cb7.internal>
2018-08-23 12:46:43 +0000

    pipeline check

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,5 +1,17 @@
 oc new-project cicd
 oc new-app jenkins-ephemeral -n cicd
-oc new-app https://github.com/wkulhanek/openshift-tasks.git --name=openshift-task -n cicd
+#oc new-app https://github.com/wkulhanek/openshift-tasks.git --name=openshift-task -n cicd
+oc new-project tasks
+oc new-app eap70-basic-s2i --param SOURCE_REPOSITORY_URL=https://github.com/kalidask/Openshft_homework --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=openshift-tasks --param APPLICATION_NAME=tasks -n tasks
+
+oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n tasks
+oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n tasks
+oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n tasks
+oc create deploymentconfig tasks --image=docker-registry.default.svc.cluster.local:5000/tasks/tasks:promoteToQA -n qa
+oc expose dc tasks --port=8080 -n qa
+oc expose svc tasks  -n qa
+
 oc create -f /opt/Openshft_homework/scripts/sample-pipeline -n cicd
 oc start-build sample-pipeline -n cicd
