7f3afff7752656a050ba6e38e94ae3e77aa54b6b
root <root@bastion.3cb7.example.opentlc.com>
2018-08-27 10:48:27 +0000

    qa added

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,13 +1,14 @@
 oc new-project cicd
+oc new-project qa
 oc new-app jenkins-ephemeral -n cicd
 #oc new-app https://github.com/wkulhanek/openshift-tasks.git --name=openshift-task -n cicd
 oc new-project tasks
 oc new-app eap70-basic-s2i --param SOURCE_REPOSITORY_URL=https://github.com/kalidask/Openshft_homework --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=openshift-tasks --param APPLICATION_NAME=tasks -n tasks
 
-oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n tasks
-oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n tasks
-oc policy add-role-to-user edit system:serviceaccount:ci-cd:jenkins -n qa
-oc policy add-role-to-group system:image-puller system:serviceaccounts:ci-cd -n qa
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n tasks
+oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n tasks
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n qa
+oc policy add-role-to-group system:image-puller system:serviceaccounts:cicd -n qa
 oc policy add-role-to-group system:image-puller system:serviceaccounts:qa -n tasks
 oc create deploymentconfig tasks --image=docker-registry.default.svc.cluster.local:5000/tasks/tasks:promoteToQA -n qa
 oc expose dc tasks --port=8080 -n qa
